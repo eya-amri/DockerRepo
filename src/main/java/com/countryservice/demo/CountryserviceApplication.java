f14f836eab8b97b59b7b4cc4eb534f892fc369d8
eya-amri <153557603+eya-amri@users.noreply.github.com>
2025-10-22 20:34:40 +0100

    Enable external Tomcat support in CountryserviceApplication
    Extend CountryserviceApplication to support external Tomcat.

--- src/main/java/com/countryservice/demo/CountryserviceApplication.java
+++ src/main/java/com/countryservice/demo/CountryserviceApplication.java
@@ -3,11 +3,20 @@ package com.countryservice.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class CountryserviceApplication {
+public class CountryserviceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryserviceApplication.class, args);
 	}
+	// Cette méthode est utilisée par Tomcat externe pour démarrer l'application
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CountryServiceApplication.class);
+    }
 
 }
+
