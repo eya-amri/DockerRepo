91f3d1e5ef65e5ea70a182db978394359876be8b
eya-amri <eya.amri@ensi-uma.tn>
2025-10-23 10:55:22 +0100

    final update

--- src/main/java/com/countryservice/demo/CountryserviceApplication.java
+++ src/main/java/com/countryservice/demo/CountryserviceApplication.java
@@ -2,18 +2,12 @@ package com.countryservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountryserviceApplication extends SpringBootServletInitializer {
+public class CountryserviceApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         SpringApplication.run(CountryserviceApplication.class, args);
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(CountryserviceApplication.class);
-    }
 }
+
